b1dc9bd035c8e778355d9f55cac563f09b43e90a
FAIZ AGHA <aghaallam316@gmail.com>
2025-01-24 15:48:37 +0530

    Movie and show time data models made

---
+++ src/main/java/com/movieReservationSystem/Movies/Entities/MovieEntity.java
@@ -0,0 +1,45 @@
+package com.movieReservationSystem.Movies.Entities;
+
+import java.util.List;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class MovieEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50, unique = true)
+    private String title;
+
+    @Column(length = 2000, unique = true)
+    private String description;
+
+    private String posterImage;
+
+    @ElementCollection
+    @CollectionTable(name = "movie_genres", joinColumns = @JoinColumn(name = "movie_id"))
+    @Column(name = "genre")
+    private List<String> genres;
+
+    @OneToMany(mappedBy = "movie")
+    private List<ShowTimeEntity> showTimes;
+}
---
+++ src/main/java/com/movieReservationSystem/Movies/Entities/ShowTimeEntity.java
@@ -0,0 +1,33 @@
+package com.movieReservationSystem.Movies.Entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class ShowTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id", nullable = false)
+    private MovieEntity movie;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+}
